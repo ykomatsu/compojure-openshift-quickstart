b78e3f2f8c030d7b4a476a0a5283ba28c910ead8
Yoshito Komatsu <ykomatsu@akaumigame.org>
2011-12-24 23:12:26 +0900

    Added some templates

--- src/main/clojure/hellocompojure/core.clj
+++ src/main/clojure/hellocompojure/core.clj
@@ -1,11 +1,21 @@
 (ns hellocompojure.core
-  (:use [compojure.core :only (GET defroutes)]
+  (:use [compojure.core :only (GET POST defroutes)]
         [compojure.route :only (not-found)]
         [compojure.handler :only (site)]
+        [clojure.java.io :only (resource)]
         [stencil.core :only (render-file)]))
 
+(defn render-root
+  []
+  (resource "root.html"))
+
+(defn render-hello
+  [name]
+  (render-file "hello.html" {:name name}))
+
 (defroutes main-routes
-  (GET "/" [] "Hello, Compojure!")
+  (GET "/" [] (render-root))
+  (POST "/hello" [name] (render-hello name))
   (not-found "Page not found"))
 
 (def app
